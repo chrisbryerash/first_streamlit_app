c8b8161de56748ebf2c8a6e53267f07ae199ad37
Chris Bryer-Ash <122454050+chrisbryerash@users.noreply.github.com>
2023-02-14 09:52:08 +0000

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.text('All fruits available:')
 streamlit.dataframe(my_fruit_list)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
