55813a9be0f729765bd1ce4879fb271055b9eba8
Chris Bryer-Ash <122454050+chrisbryerash@users.noreply.github.com>
2023-02-14 12:38:00 +0000

    Update streamlit_app.py
    L to l so many Ls

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
   streamlit.error()
 
 streamlit.header("View our Fruit List - Add your favourites!")
-def get_fruit_Load_list():
+def get_fruit_load_list():
   with my_cur.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
