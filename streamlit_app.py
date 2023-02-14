6379172d96106a86e1a1804f663e7b33a75d55e9
Chris Bryer-Ash <122454050+chrisbryerash@users.noreply.github.com>
2023-02-14 10:06:19 +0000

    Update streamlit_app.py
    api info on specific fruit request input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,14 @@ streamlit.dataframe(fruits_to_show)
 streamlit.text('All fruits available:')
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+
+# unpacks the json file
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays in a dataframe, probably could be skipped
+streamlit.dataframe(fruityvice_normalized)
